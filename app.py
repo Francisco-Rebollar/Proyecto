e81835efdf5965351ceaf759cc7b05b6e0d4394a
Francisco Rebollar <francis.ko.05.07@gmail.com>
2024-03-10 11:02:03 -0600

    actualizacion 3

--- app.py
+++ app.py
@@ -2,16 +2,36 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
         
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-        
+    st.write('Histograma del odómetro de los anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer", title='Histograma del Odómetro', labels={'odometer': 'Odómetro'})
+
+    # personalizar colores y formato
+    fig.update_layout(
+        xaxis_title='Odómetro',
+        yaxis_title='Frecuencia',
+        template='plotly_dark'  # puedes cambiar a otros temas disponibles en Plotly
+    )
+
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Construir gráfico de dispersión')  # crear un botón
+
+if disp_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Gráfico de dispersión entre el odómetro y el precio de los anuncios de venta de coches')
+
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price", title='Gráfico de Dispersión',
+                      labels={'odometer': 'Odómetro', 'price': 'Precio'})
+
+
+    
 
